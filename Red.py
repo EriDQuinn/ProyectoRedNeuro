ffca6d6b202e80dff2d601f9f28b9a9a4f258a87
ErikaDiaz <33587900+EriDQuinn@users.noreply.github.com>
2018-11-13 00:31:33 -0600

    Create Red.py

---
+++ Red.py
@@ -0,0 +1,6 @@
+#para hacer y entrenar la red
+#USANDO PyBrain
+
+from pybrain.datasets import SupervisedDataSet
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.supervised.trainers import BackpropTrainer
